a1338a799a4f61d851bdc58eb16fbbeb336df6e8
Pascal Hurni <phi@pragmaticpurist.org>
2023-11-16 17:27:07 +0100

    Add fields to the Article migration

--- database/migrations/2023_11_16_131645_create_articles_table.php
+++ database/migrations/2023_11_16_131645_create_articles_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('articles', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->string('body');
+            $table->datetime('published_at')->nullable();
             $table->timestamps();
         });
     }
