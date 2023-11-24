e9e00ad79a79a28f152662d8ced9ee776c9a5f65
Pascal Hurni <phi@pragmaticpurist.org>
2023-11-24 14:13:29 +0100

    Add a layout for all views

--- resources/views/articles/index.blade.php
+++ resources/views/articles/index.blade.php
@@ -1,6 +1,10 @@
-<h1>Voici les articles</h1>
-<ul>
-@foreach ($articles as $article)
-<li><a href="{{ route('articles.show', $article) }}">{{ $article->title }}</a></li>
-@endforeach
-</ul>
+@extends('layout')
+
+@section('content')
+    <h1>Voici les articles</h1>
+    <ul>
+    @foreach ($articles as $article)
+        <li><a href="{{ route('articles.show', $article) }}">{{ $article->title }}</a></li>
+    @endforeach
+    </ul>
+@endsection
