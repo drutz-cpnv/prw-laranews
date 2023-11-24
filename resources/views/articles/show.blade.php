0b65e02d5aafb647dbf4fe64be7e9f0b01674a13
Pascal Hurni <phi@pragmaticpurist.org>
2023-11-24 14:55:25 +0100

    Use the layout for the Article show action

--- resources/views/articles/show.blade.php
+++ resources/views/articles/show.blade.php
@@ -1,11 +1,15 @@
-<h2>{{ $article->title }}</h2>
-<article>
-    {{ $article->body }}
-</article>
-<form method="POST" action="{{ route('articles.destroy', $article) }}">
-    @csrf
-    @method('DELETE')
-    <input type="submit" value="Supprimer l'article">
-</form>
+@extends('layout')
 
-<a href="{{ route('articles.edit', $article) }}">Modifier cet article</a>
+@section('content')
+    <h2>{{ $article->title }}</h2>
+    <article>
+        {{ $article->body }}
+    </article>
+    <form method="POST" action="{{ route('articles.destroy', $article) }}">
+        @csrf
+        @method('DELETE')
+        <input type="submit" value="Supprimer l'article">
+    </form>
+
+    <a href="{{ route('articles.edit', $article) }}">Modifier cet article</a>
+@endsection
