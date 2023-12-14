00a1c1aa2919eeb6deaf1999a1152d672a0c5bbc
Pascal Hurni <phi@pragmaticpurist.org>
2023-12-14 14:28:10 +0100

    Add error reporting for Article forms

--- resources/views/articles/fields.blade.php
+++ resources/views/articles/fields.blade.php
@@ -1,4 +1,10 @@
 <input type="text" name="title" value="{{ $article->title }}">
+@error('title')
+  <div class="alert alert-danger">{{ $message }}</div>
+@enderror
 <textarea name="body">
     {{ $article->body }}
 </textarea>
+@error('body')
+  <div class="alert alert-danger">{{ $message }}</div>
+@enderror
